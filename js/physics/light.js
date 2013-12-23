dc7d4b93a8532ff516e6100ef43c45ca0af5b305
Mailson Menezes <mailson.menezes@openbossa.org>
2013-12-23 12:16:37 -0300

    Remove unsused stuff from Light
    Signed-off-by: Mailson Menezes <mailson.menezes@openbossa.org>

--- js/physics/light.js
+++ js/physics/light.js
@@ -4,33 +4,9 @@
  * @class Light
  * @param pos {Object} Source position
  * @param points {Array} An array of two points. They are the initial points of the light
- * @param color {String} The color of the light
  */
-var Light = function (pos, points, color) {
-    this.world = null;
-
+var Light = function (pos, points) {
     this.pos = pos;
-    this.color = color;
     this.points = points;
     this.rays = [];
-};
-
-/**
- * Paints the light into a context
- *
- * @method paint
- * @param context {CanvasRenderingContext2D} A canvas context 2D
- */
-Light.prototype.paint = function (context, rays) {
-    context.fillStyle = this.color;
-    context.beginPath();
-    context.moveTo(this.pos.x, this.pos.y);
-    for (var i = 0; i < rays.data.length; i++) {
-        if (!rays.data[i].isFinite()) // XXX
-            rays.data[i].p2 = this.world.intersectionPoint(rays.data[i]);
-
-        context.lineTo(rays.data[i].oriented().p1.x, rays.data[i].oriented().p1.y);
-        context.lineTo(rays.data[i].oriented().p2.x, rays.data[i].oriented().p2.y);
-    }
-    context.fill();
 };
