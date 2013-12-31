335b39439ddb4b7a6b5540638cf1f91580269b74
Mailson Menezes <mailson.menezes@openbossa.org>
2013-12-31 12:21:42 -0300

    Add test levels
    Whenever we find a bug, we should add a simple bugged level to this
    file.
    
    Signed-off-by: Mailson Menezes <mailson.menezes@openbossa.org>

---
+++ tests/testlevels.js
@@ -0,0 +1,9 @@
+var TestLevels = {};
+
+TestLevels.levels = [
+    [
+        {type: "light", rays: [{p1: {x: 110, y: 97.33333}, vector: {x: 300, y: -80}}, {p1: {x: 110, y: 101.66666}, vector: {x: 300, y: 50}}], color: "yellow"},
+        {type: "mirror", p1: {x: 250, y: 80}, p2: {x: 250, y: 120}},
+        {type: "opaque", p1: {x: 250, y: 40}, p2: {x: 250, y: 70}}
+    ],
+];
