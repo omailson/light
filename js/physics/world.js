06c4190bbfe5ec2b06da2298b5dd2c44fc045441
Mailson Menezes <mailson.menezes@openbossa.org>
2014-01-03 13:51:07 -0300

    [physics] computeReflection now returns only one RayCollection
    Turns out only one RayCollection is needed for this.
    
    This also fixes a bug where sometimes the corners of the canvas weren't
    painted.
    
    Signed-off-by: Mailson Menezes <mailson.menezes@openbossa.org>

--- js/physics/world.js
+++ js/physics/world.js
@@ -127,7 +127,8 @@ World.prototype.computeRays = function (light) {
         for (var j = 0; j < this._objects.length; j++) {
             if (this._objects[j] instanceof Mirror) {
                 var newRays = this._objects[j].computeReflection(rays[i]);
-                rays = rays.concat(newRays);
+                if (newRays)
+                    rays.push(newRays);
             }
         }
     }
