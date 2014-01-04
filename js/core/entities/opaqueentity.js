ecc530d7aff55808fb6dc9a19e115abd50694363
Mailson Menezes <mailson.menezes@openbossa.org>
2014-01-04 14:21:54 -0300

    [core] Use LineSegmentEntity to deal with Opaque/Mirror interactivity
    Signed-off-by: Mailson Menezes <mailson.menezes@openbossa.org>

--- js/core/entities/opaqueentity.js
+++ js/core/entities/opaqueentity.js
@@ -2,8 +2,7 @@ var OpaqueEntity = function (params) {
     this.p1 = params.p1;
     this.p2 = params.p2;
 
-    this._grabbed = false;
-    this._grabbedPoint = 0;
+    this._interactiveHandler = new LineSegmentEntity(params);
 };
 
 inherits(OpaqueEntity, InteractiveEntity);
@@ -19,36 +18,19 @@ OpaqueEntity.prototype.initPhysics = function (physicsWorld) {
 };
 
 OpaqueEntity.prototype.acceptsInputEvent = function (input) {
-    var parentCall = InteractiveEntity.prototype.acceptsInputEvent.call(this, input);
-    if (parentCall)
-        return true;
-
-    if (this._grabbed)
-        return true;
-
-    if (MathHelper.dist(this.p1, input.position) < 4
-            || MathHelper.dist(this.p2, input.position) < 4)
-        return true;
-
-    return false;
+    return this._interactiveHandler.acceptsInputEvent(input);
 };
 
 OpaqueEntity.prototype.onPress = function (ev) {
-    this._grabbed = true;
-    if (MathHelper.dist(this.p1, ev.position) < MathHelper.dist(this.p2, ev.position))
-        this._grabbedPoint = 1;
-    else
-        this._grabbedPoint = 2;
+    this._interactiveHandler.onPress(ev);
 };
 
 OpaqueEntity.prototype.onMove = function (ev) {
-    if (this._grabbedPoint === 1)
-        this.body.p1 = {x: ev.position.x, y: ev.position.y };
-    else if (this._grabbedPoint === 2)
-        this.body.p2 = {x: ev.position.x, y: ev.position.y };
+    this._interactiveHandler.onMove(ev);
+    this.body.p1 = this._interactiveHandler.p1();
+    this.body.p2 = this._interactiveHandler.p2();
 };
 
 OpaqueEntity.prototype.onRelease = function (ev) {
-    this._grabbed = false;
-    this._grabbedPoint = 0;
+    this._interactiveHandler.onRelease(ev);
 };
