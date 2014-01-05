282a43fa1c6afd48a33c08ad4ddb4c3dc45d18f7
Mailson Menezes <mailson.menezes@openbossa.org>
2014-01-04 23:41:27 -0300

    Add test fixed previously
    The test was fixed after 06c4190b
    
    Signed-off-by: Mailson Menezes <mailson.menezes@openbossa.org>

--- tests/testlevels.js
+++ tests/testlevels.js
@@ -82,4 +82,8 @@ TestLevels.levels = [
         {type: "mirror", p1: {x: 323, y: 74}, p2: {x: 323, y: 119}},
         {type: "mirror", p1: {x: 50, y: 76}, p2: {x: 50, y: 121}}
     ],
+    [
+        {type: "light", rays: [{p1: {x: 110, y: 97.33333}, vector: {x: 300, y: -80}}, {p1: {x: 110, y: 101.66666}, vector: {x: 300, y: 50}}], color: "yellow"},
+        {type: "mirror", p1: {x: 250, y: 80}, p2: {x: 310, y: 72}},
+    ],
 ];
