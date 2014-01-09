46bc818e858be816b5c28d3b082c3326b25048f5
Mailson Menezes <mailson.menezes@openbossa.org>
2014-01-09 17:48:33 -0300

    [utils] Add function to perform a deep copy of an object
    Signed-off-by: Mailson Menezes <mailson.menezes@openbossa.org>

--- js/utils/object.js
+++ js/utils/object.js
@@ -66,3 +66,43 @@ var inherits = function (target, source) {
         target.prototype[property] = source.prototype[property];
     }
 };
+
+/**
+ * Performs a deep copy of an object
+ *
+ * Retrieve from: http://stackoverflow.com/a/728694/556192
+ *
+ * @param obj {Object} the object to be cloned
+ * @return {Object} a copy of the object
+ */
+var deepCopy = function (obj) {
+    // Handle the 3 simple types, and null or undefined
+    if (null == obj || "object" != typeof obj) return obj;
+
+    // Handle Date
+    if (obj instanceof Date) {
+        var copy = new Date();
+        copy.setTime(obj.getTime());
+        return copy;
+    }
+
+    // Handle Array
+    if (obj instanceof Array) {
+        var copy = [];
+        for (var i = 0, len = obj.length; i < len; i++) {
+            copy[i] = deepCopy(obj[i]);
+        }
+        return copy;
+    }
+
+    // Handle Object
+    if (obj instanceof Object) {
+        var copy = {};
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = deepCopy(obj[attr]);
+        }
+        return copy;
+    }
+
+    throw new Error("Unable to copy obj! Its type isn't supported.");
+}
