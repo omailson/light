d75d03ddd143435dd1d50821b5c6bb8f254d2324
Mailson Menezes <mailson.menezes@openbossa.org>
2014-01-14 11:55:01 -0300

    Create levels file
    Signed-off-by: Mailson Menezes <mailson.menezes@openbossa.org>

---
+++ data/levels.js
@@ -0,0 +1,10 @@
+var Levels = {};
+
+Levels.data = [
+    {
+        sprites: [
+            {type: "lightbox", pos: {x: 150, y: 200}, color: "yellow", structure: {width: 4, height: 4, map: [1, 1,1, 2, 2,2, 1, 1,1, 1, 1,1]}},
+            {type: "target", pos: {x: 175, y: 50}, colors: ["yellow"]}
+        ]
+    }
+];
--- js/ui/main.js
+++ js/ui/main.js
@@ -44,15 +44,7 @@ Main.prototype._createPages = function () {
 };
 
 Main.prototype._loadLevels = function () {
-    var levelData = {
-        sprites: [
-            {type: "light", rays: [{p1: {x: 110, y: 97.33333}, vector: {x: 300, y: -80}}, {p1: {x: 110, y: 101.66666}, vector: {x: 300, y: 50}}], color: "yellow"},
-            {type: "mirror", p1: {x: 250, y: 80}, p2: {x: 250, y: 120}},
-            {type: "opaque", p1: {x: 250, y: 40}, p2: {x: 250, y: 70}}
-        ]
-    };
-
-    this._levelModel = [levelData];
+    this._levelModel = Levels.data;
 };
 
 Main.prototype._goToMainPage = function () {
