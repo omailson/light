d096d386ab47077617e26a2f82fb77b5d88fa3a4
Mailson Menezes <mailson.menezes@openbossa.org>
2014-01-15 18:30:29 -0300

    Add third level
    Signed-off-by: Mailson Menezes <mailson.menezes@openbossa.org>

--- data/levels.js
+++ data/levels.js
@@ -16,4 +16,13 @@ Levels.data = [
             {type: "wall", pos: {x: 600, y: 130}, structure: {orientation: "vertical", map: [1,1,1,1,1,1,1,1,1,1,1,1]}}
         ]
     },
+    {
+        sprites: [
+            {type: "lightbox", pos: {x: 400, y: 500}, color: "yellow", structure: {width: 4, height: 4, map: [1, 1,1, 1, 1,1, 1, 1,1, 1, 1,1]}},
+            {type: "target", pos: {x: 400, y: 40}, colors: ["yellow"]},
+            {type: "wall", pos: {x: 191, y: 100}, structure: {orientation: "horizontal", map: [1,1,1,1,1,1,1,1,1,1,1,1,1,1]}},
+            {type: "wall", pos: {x: 320, y: 300}, structure: {orientation: "horizontal", map: [2,2,2,2,2]}},
+            {type: "mirror", p1: {x: 214, y: 300}, p2: {x: 214, y: 265}}
+        ]
+    },
 ];
