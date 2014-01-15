0fd3aaa4f4330b1d901adf9363d6e9f3858f20ec
Mailson Menezes <mailson.menezes@openbossa.org>
2014-01-15 15:21:46 -0300

    Add second level
    Signed-off-by: Mailson Menezes <mailson.menezes@openbossa.org>

--- data/levels.js
+++ data/levels.js
@@ -6,5 +6,14 @@ Levels.data = [
             {type: "lightbox", pos: {x: 80, y: 520}, color: "yellow", structure: {width: 4, height: 4, map: [1, 1,1, 1, 1,1, 1, 1,1, 1, 1,1]}},
             {type: "target", pos: {x: 690, y: 70}, colors: ["yellow"]}
         ]
-    }
+    },
+    {
+        sprites: [
+            {type: "lightbox", pos: {x: 80, y: 300}, color: "yellow", structure: {width: 4, height: 5, map: [1, 1,1, 1, 1,1,1, 1, 1,1, 1, 1,1,1]}},
+            {type: "target", pos: {x: 690, y: 300}, colors: ["yellow"]},
+            {type: "wall", pos: {x: 200, y: 130}, structure: {orientation: "vertical", map: [1,1,1,1,1,1,1,1,1,1,1,1]}},
+            {type: "wall", pos: {x: 400, y: 130}, structure: {orientation: "vertical", map: [1,1,1,1,1,1,1,1,1,1,1,1]}},
+            {type: "wall", pos: {x: 600, y: 130}, structure: {orientation: "vertical", map: [1,1,1,1,1,1,1,1,1,1,1,1]}}
+        ]
+    },
 ];
